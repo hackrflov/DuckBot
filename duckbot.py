42374c9a0ebfd08f850f97a4f7e296127c259803
hackrflov <hackrflov@gmail.com>
2021-03-17 16:12:53 +0800

    support KDA-shill event

--- duckbot.py
+++ duckbot.py
@@ -1,4 +1,5 @@
-from wechaty import Wechaty, Message
+import asyncio
+from wechaty import Wechaty, Message, UrlLink
 import requests
 import time
 from wechaty_puppet import get_logger
@@ -9,6 +10,7 @@ import re
 import cv2
 import random
 import threading
+import concurrent.futures
 
 log = get_logger('mybot')
 
@@ -29,7 +31,7 @@ class DuckBot(Wechaty):
     }
 
     def set_db(self, db):
-        self.duck_db = db
+        self.db = db
 
     def set_crawler(self, crawler):
         self.crawler = crawler
@@ -51,6 +53,7 @@ class DuckBot(Wechaty):
 
         # 基本信息
         msg_text = msg.text()
+        msg_text_inline = msg.text().replace('\n', '')
         msg_type = msg.message_type()
         msg_id = msg.message_id
         if msg_type == 6:
@@ -72,14 +75,14 @@ class DuckBot(Wechaty):
         values = ','.join(['\'{}\''.format(str(v)) for v in insert_data.values()])
         sql = 'INSERT INTO {table}({keys}) VALUES({values})'.format(table='msg_records', keys=keys, values=values)
         try:
-            self.duck_db.cursor.execute(sql)
-            self.duck_db.db.commit()
+            self.db.cursor.execute(sql)
+            self.db.db.commit()
         except Exception as e:
             log.error(e)
-            self.duck_db.db.rollback()
+            self.db.db.rollback()
 
         # 鉴定是否为广告
-        if room and not re.match(r'.*(kda|kadena|可达).*', msg_text.lower()):
+        if room and not re.match(r'.*(kda|kadena|可达).*', msg_text_inline.lower()):
             is_ad = False
             if msg_type == 4:
                 pass
@@ -98,14 +101,14 @@ class DuckBot(Wechaty):
                     is_ad = True
                 os.remove(img_path)
             elif msg_type == 6:
-                if len(msg_text) >= 50 and re.match(r'.*(http|空投|撸).*', msg_text):
+                if len(msg_text) >= 50 and re.match(r'.*(http|空投|撸).*', msg_text) and not re.match(r'.*bihu\.com.*', msg_text):
                     log.info('ad detected text')
                     is_ad = True
 
             if is_ad:
                 sql = """SELECT count(*) as msg_cnt FROM {} where contact_id = '{}'""".format('msg_records', talker.contact_id)
-                self.duck_db.cursor.execute(sql)
-                if list(self.duck_db.cursor.fetchall())[0]['msg_cnt'] < 10:
+                self.db.cursor.execute(sql)
+                if list(self.db.cursor.fetchall())[0]['msg_cnt'] < 10:
                     q_list = list(self.ad_qa_dict.keys())
                     question = q_list[random.randrange(0, len(q_list))]
                     threading.Thread(target=self.kick_member, args=(talker, room, question)).start()
@@ -118,8 +121,8 @@ class DuckBot(Wechaty):
             if 'c/new' in msg_text:
                 if '上一条' in msg_text:
                     sql = """SELECT msg_id FROM {} where contact_id = '{}' order by created_at desc""".format('msg_records', self.god_contact_id)
-                    self.duck_db.cursor.execute(sql)
-                    refer_msg_id = list(self.duck_db.cursor.fetchall())[1]['msg_id']
+                    self.db.cursor.execute(sql)
+                    refer_msg_id = list(self.db.cursor.fetchall())[1]['msg_id']
                     title = msg_text
                 else:
                     root = ET.fromstring(msg_text)
@@ -136,21 +139,21 @@ class DuckBot(Wechaty):
                 values = ','.join(['\'{}\''.format(str(v)) for v in insert_data.values()])
                 sql = 'INSERT INTO {table}({keys}) VALUES({values})'.format(table='materials', keys=keys, values=values)
                 try:
-                    self.duck_db.cursor.execute(sql)
-                    self.duck_db.db.commit()
+                    self.db.cursor.execute(sql)
+                    self.db.db.commit()
                     log.info('INSERT succesfully: {}'.format(sql))
                 except Exception as e:
                     log.error(e)
-                    self.duck_db.db.rollback()
+                    self.db.db.rollback()
 
                 reply = '指令已储存，关键词：{}'.format(keyword)
                 await msg.say(reply)
 
             elif 'c/list' in msg_text:
                 sql = """SELECT keyword FROM {} group by keyword""".format('materials')
-                self.duck_db.cursor.execute(sql)
+                self.db.cursor.execute(sql)
                 reply = '现有指令'
-                for row in self.duck_db.cursor.fetchall():
+                for row in self.db.cursor.fetchall():
                     reply += '\n' + row['keyword']
 
                 await msg.say(reply)
@@ -165,8 +168,8 @@ class DuckBot(Wechaty):
                 sql = """SELECT contact_id, count(*) as msg_cnt
                         FROM {} where room_id = '{}' and created_at >= '{}'
                         group by contact_id""".format('msg_records', room.room_id, start_dt)
-                self.duck_db.cursor.execute(sql)
-                records = [row['contact_id'] for row in self.duck_db.cursor.fetchall()]
+                self.db.cursor.execute(sql)
+                records = [row['contact_id'] for row in self.db.cursor.fetchall()]
                 member_list = await room.member_list()
                 reply = '今日群内未发言成员：'
                 for member in member_list:
@@ -178,11 +181,115 @@ class DuckBot(Wechaty):
                     reply += '\n如果没法保持每天活跃，会被移出此群哦~'
                 await msg.say(reply)
 
+            elif 'c/kdashill' in msg_text:
+                today = datetime.now()
+                start_dt = datetime(today.year, today.month, today.day)
+                sql = """SELECT contact_id, max(contact_name) as contact_name, count(*) as score
+                        FROM {} group by contact_id""".format('kdashill_records')
+                self.db.cursor.execute(sql)
+                reply = '可达秀 [KDA-Shill] 活动积分排名\n'
+                rank = 0
+                for row in self.db.cursor.fetchall():
+                    rank += 1
+                    reply += '\n' + '{} - {}: {}分'.format(rank, row['contact_name'], row['score'])
+
+                await room.say(reply)
+
             return
 
 
+        # 推荐活动
+        if room and re.match(r'.*bihu\.com.*', msg_text_inline):
+            match_results = re.search(r'(?<=bihu\.com/shortcontent/)\d{1,}', msg_text_inline.lower())
+            if match_results:
+                content_id = match_results[0]
+                log.info('get content id = {}'.format(content_id))
+                data = None
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future = executor.submit(self.crawler.fetch_bihu, content_id)
+                    data = future.result()
+
+                log.info('fetched bihu data = {}'.format(data))
+                if data:
+                    content_inline = data['content'].replace('\n', '')
+                    post_date = datetime.fromtimestamp(data['createTS'] // 1000)
+                    if not re.match(r'.*(kda|kadena|可达).*', content_inline.lower()):
+                        reply = '未发现Kadena相关信息哦~'
+                        reply = reply + ' @{}'.format(talker.name)
+                        await room.say(reply, mention_ids=[talker.contact_id])
+                        return
+                    elif datetime.now() - post_date > timedelta(days=1):
+                        reply = '文章太过久远，不能算入哦~'
+                        reply = reply + ' @{}'.format(talker.name)
+                        await room.say(reply, mention_ids=[talker.contact_id])
+                        return
+
+                    # 展示
+                    reply_url = 'https://m.bihu.com/shortcontent/{}'.format(content_id)
+                    reply_title = '可达社区 @ {}'.format(talker.name)
+                    if 'imageUrlList' in data:
+                        prefix = 'https://oss-cdn1.bihu-static.com/'
+                        reply_thumbnail = prefix + data['imageUrlList'][0]
+                    else:
+                        reply_thumbnail = ''
+                    reply_description = data['content'][:60]
+                    log.info('to create url_link: {},{},{},{}'.format(reply_url, reply_title, reply_thumbnail, reply_description))
+                    reply_link = UrlLink.create(reply_url, reply_title, reply_thumbnail, reply_description)
+                    log.info('url created')
+                    await room.say(reply_link)
+
+                    # 查重
+                    sql = """SELECT * FROM {} where content_id = '{}'""".format('kdashill_records', content_id)
+                    self.db.cursor.execute(sql)
+                    if len(list(self.db.cursor.fetchall())):
+                        reply = '文章已有录入哦~'
+                        reply = reply + ' @{}'.format(talker.name)
+                        await room.say(reply, mention_ids=[talker.contact_id])
+                        return
+
+                    # 记分
+                    insert_data = {
+                        'content_id': content_id,
+                        'contact_id': talker.contact_id,
+                        'contact_name': talker.name,
+                        'room_id': room.room_id,
+                        'created_at': datetime.now()
+                    }
+                    keys = ','.join(['`{}`'.format(str(v)) for v in insert_data.keys()])
+                    values = ','.join(['\'{}\''.format(str(v)) for v in insert_data.values()])
+                    sql = 'INSERT INTO {table}({keys}) VALUES({values})'.format(table='kdashill_records', keys=keys, values=values)
+                    try:
+                        self.db.cursor.execute(sql)
+                        self.db.db.commit()
+                        log.info('INSERT succesfully: {}'.format(sql))
+                    except Exception as e:
+                        log.error(e)
+                        self.db.db.rollback()
+
+                    # 报积分
+                    sql = """SELECT contact_id, count(*) as score FROM {} group by contact_id order by score desc""".format('kdashill_records')
+                    self.db.cursor.execute(sql)
+                    rank = 0
+                    for row in self.db.cursor.fetchall():
+                        rank += 1
+                        if row['contact_id'] == talker.contact_id:
+                            reply = '感谢参加可达秀 [KDA-Shill] 活动！🍻\n'
+                            reply += '您当前积分为{}，排名为{}'.format(row['score'], rank)
+                            break
+
+                    reply = reply + ' @{}'.format(talker.name)
+                    await room.say(reply, mention_ids=[talker.contact_id])
+                    return
+
+                else:
+                    reply = '未查到相关网页'
+                    reply = reply + ' @{}'.format(talker.name)
+                    await room.say(reply, mention_ids=[talker.contact_id])
+                    return
+
+
         # 关键词回复
-        if re.match(r'(资料|学习|学习资料|新人)', msg_text):
+        if re.match(r'(资料|学习|学习资料|新人)', msg_text_inline):
             await self.send_msg_with_keyword('新手指南', contact=talker, room=room)
             return
 
@@ -216,10 +323,10 @@ class DuckBot(Wechaty):
         time.sleep(15)
         start_dt = datetime.now() - timedelta(minutes=1)
         sql = """SELECT msg_text FROM {} where contact_id = '{}' and created_at >= '{}'""".format('msg_records', talker.contact_id, start_dt)
-        self.duck_db.cursor.execute(sql)
+        self.db.cursor.execute(sql)
         right_answer = self.ad_qa_dict[question]
         has_correct_answer = False
-        for row in self.duck_db.cursor.fetchall():
+        for row in self.db.cursor.fetchall():
             log.info('fetch {}, answer = {}'.format(row['msg_text'], right_answer))
             if right_answer.lower() in row['msg_text'].lower():
                 has_correct_answer = True
@@ -242,8 +349,8 @@ class DuckBot(Wechaty):
 
     async def send_msg_with_keyword(self, keyword, contact=None, room=None):
         sql = """SELECT msg_id FROM {} where keyword = '{}' order by created_at desc limit 1""".format('materials', keyword)
-        self.duck_db.cursor.execute(sql)
-        msg_id = list(self.duck_db.cursor.fetchall())[0]['msg_id']
+        self.db.cursor.execute(sql)
+        msg_id = list(self.db.cursor.fetchall())[0]['msg_id']
         refer_msg = self.Message(message_id=msg_id)
         await refer_msg.ready()
         #await self.forward(msg, refer_msg)
--- duckcrawler.py
+++ duckcrawler.py
@@ -3,7 +3,6 @@ import requests
 class DuckCrawler:
 
     def fetch_bihu(self, content_id):
-        content_id = '1085179516'
         url = 'https://gw.bihu.com/api/content/shortContent/{}'.format(content_id)
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
@@ -11,4 +10,7 @@ class DuckCrawler:
         }
         res = requests.get(url, headers=headers)
         data = res.json()
-        return data
+        if data['code'] == 1:
+            return data['data']
+        else:
+            return None
